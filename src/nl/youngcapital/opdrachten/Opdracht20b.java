0c81f6f6c5af6720a1323b6411d38267209af1ea
Student <Student>
2016-03-16 09:32:17 +0100

    Opdracht 20b af

--- src/nl/youngcapital/opdrachten/Opdracht20b.java
+++ src/nl/youngcapital/opdrachten/Opdracht20b.java
@@ -7,10 +7,24 @@ public class Opdracht20b {
 		System.out.println("Geef een letter op");
 		Scanner s = new Scanner(System.in);
 		char input = s.next().charAt(0);
-		
-		char A = 'a';
-		int pos = A -= input;
-		
-		System.out.println(pos);
+		int inputVal = Integer.valueOf(input);
+		int inputPos;
+
+		System.out.println(inputVal); //debug
+
+
+		if(inputVal >= 97){ //kleine letter
+			char firstLetter = 'a';
+			int firstVal = Integer.valueOf(firstLetter);
+			inputPos = inputVal - firstVal;
+
+		}
+
+		else{	//hoofdletter
+			char firstCapital = 'A';
+			int firstCapVal = Integer.valueOf(firstCapital);
+			inputPos = inputVal - firstCapVal;
+		}
+		System.out.println(inputPos + 1);
 	}
 }
