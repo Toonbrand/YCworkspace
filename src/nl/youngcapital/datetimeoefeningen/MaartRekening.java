7d14790023cfc730be217f79093c92edfc5030a2
Student <Student>
2016-03-25 15:48:55 +0100

    Einde van de dag push.

---
+++ src/nl/youngcapital/datetimeoefeningen/MaartRekening.java
@@ -0,0 +1,10 @@
+package nl.youngcapital.datetimeoefeningen;
+
+import java.time.LocalDate;
+
+public class MaartRekening {
+	public static void main(String[] args){
+		
+		LocalDate eersteMaart = LocalDate.of(2015, 1, 1);
+	}
+}
